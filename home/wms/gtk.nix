ee4f545412f6d6f19d75a6c41b45772a409b5c9d
KasperJSdeVries <89792927+KasperJSdeVries@users.noreply.github.com>
2024-01-12 12:14:09 +0000

    Set desktop theme to dark & add chrome

---
+++ home/wms/gtk.nix
@@ -0,0 +1,16 @@
+{pkgs, ...}: {
+  home.packages = [pkgs.dconf];
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Catppuccin-Mocha-Compact-Pink-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        accents = ["pink"];
+        size = "compact";
+        tweaks = ["rimless" "black"];
+        variant = "mocha";
+      };
+    };
+  };
+}
