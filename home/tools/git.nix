ff0fb4a898626c98ae98aeae7d446c4e7467fbdc
KasperJSdeVries <89792927+KasperJSdeVries@users.noreply.github.com>
2024-01-12 15:55:29 +0000

    Add the gh package

--- home/tools/git.nix
+++ home/tools/git.nix
@@ -1,8 +1,16 @@
-{
+{pkgs, ...}: {
   programs.git = {
     enable = true;
 
     userName = "KasperJSdeVries";
     userEmail = "89792927+KasperJSdeVries@users.noreply.github.com";
+  };
+
+  programs.gh = {
+    enable = true;
+
+    extensions = [ pkgs.act ];
+
+    settings.editor = "nvim";
   };
 }
