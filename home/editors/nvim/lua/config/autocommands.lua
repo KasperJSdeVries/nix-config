cdfcb69a27195fb96854785714185cbfd694af6f
KasperJSdeVries <89792927+KasperJSdeVries@users.noreply.github.com>
2024-04-17 15:08:45 +0200

    NeoVim: Add .comp files to glsl file detection.

--- home/editors/nvim/lua/config/autocommands.lua
+++ home/editors/nvim/lua/config/autocommands.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead', }, {
-    pattern = { "*.frag", "*.vert", "*.glsl" },
+    pattern = { "*.frag", "*.vert", "*.glsl", "*.comp" },
     callback = function()
         local buf = vim.api.nvim_get_current_buf()
         vim.api.nvim_buf_set_option(buf, "filetype", "glsl")
