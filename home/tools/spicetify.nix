e4c6020365e2f7fd3d564a4e16c9124ec9258414
KasperJSdeVries <89792927+KasperJSdeVries@users.noreply.github.com>
2024-07-04 04:53:05 +0200

    Spicetify: add genres extension and stats and library apps

--- home/tools/spicetify.nix
+++ home/tools/spicetify.nix
@@ -14,6 +14,21 @@
     url = "https://github.com/adufr/spicetify-extensions.git";
     rev = "4af311ecf572cd112f3b954c1d1446eebbda0180";
   };
+
+  genresSrc = fetchGit {
+    url = "https://github.com/LucasOe/spicetify-genres.git";
+    rev = "be69d720332e4ce695574b01604e08e0404e7aa3";
+  };
+
+  statsSrc = pkgs.fetchzip {
+    url = "https://github.com/harbassan/spicetify-apps/releases/download/stats-v0.3.3/spicetify-stats.release.zip";
+    sha256 = "0prm8qs9zr7ly8z33iyfnsw52dhp55bkvdw00fhgdw45wjfw7gbf";
+  };
+
+  librarySrc = pkgs.fetchzip {
+    url = "https://github.com/harbassan/spicetify-apps/releases/download/library-v0.1.1/spicetify-library.release.zip";
+    sha256 = "1yfnkh68g1s9ch8mf9sw9dwz80bgqkvkbxqffyxz7p4nm03133f5";
+  };
 in {
   imports = [spicetify-nix.homeManagerModules.default];
 
@@ -22,15 +37,21 @@ in {
     theme = spicePkgs.themes.catppuccin;
     colorScheme = "mocha";
 
-    ## NOTE: Enable to browse extensions
-
-    # enabledCustomApps = with spicePkgs.apps; [
-    #   marketplace
-    # ];
+    enabledCustomApps = [
+      {
+        name = "library";
+        src = "${librarySrc}";
+        appendName = false;
+      }
+      {
+        name = "stats";
+        src = "${statsSrc}";
+        appendName = false;
+      }
+    ];
 
     enabledExtensions = with spicePkgs.extensions; [
       featureShuffle
-      #fullAppDisplay
       hidePodcasts
       historyShortcut
       keyboardShortcut
@@ -49,6 +70,10 @@ in {
       {
         src = "${adufrExtensions}/quick-add-to-queue/dist";
         filename = "quick-add-to-queue.js";
+      }
+      {
+        src = "${genresSrc}/dist";
+        filename = "whatsThatGenre.js";
       }
     ];
   };
