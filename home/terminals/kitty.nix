5a9aa7bf2f9d20b724837caf60010d0a68a6c8ff
KasperJSdeVries <89792927+KasperJSdeVries@users.noreply.github.com>
2024-09-19 22:27:55 +0200

    use tmux + fix mini_comment

--- home/terminals/kitty.nix
+++ home/terminals/kitty.nix
@@ -8,18 +8,18 @@
     shellIntegration.enableFishIntegration = true;
     settings = {
       enable_audio_bell = "no";
-      #shell = "${pkgs.tmux}/bin/tmux new-session -A -D -s main";
-      shell = "${pkgs.zsh}/bin/zsh";
+      shell = "${pkgs.tmux}/bin/tmux new-session -A -D -s main";
+      #shell = "${pkgs.zsh}/bin/zsh";
       background_opacity = "0.7";
     };
-    keybindings = {
-      "ctrl+shift+h" = "previous_tab";
-      "ctrl+shift+l" = "next_tab";
-      "ctrl+alt+enter" = "launch --cwd=current";
-      "ctrl+alt+t" = "detach_window new-tab";
-      "ctrl+alt+l" = "next_layout";
-      "ctrl+alt+p" = "previous_layout";
-    };
+    #keybindings = {
+    #  "ctrl+shift+h" = "previous_tab";
+    #  "ctrl+shift+l" = "next_tab";
+    #  "ctrl+alt+enter" = "launch --cwd=current";
+    #  "ctrl+alt+t" = "detach_window new-tab";
+    #  "ctrl+alt+l" = "next_layout";
+    #  "ctrl+alt+p" = "previous_layout";
+    #};
     theme = "Catppuccin-Mocha";
   };
 }
